a5ccdf12bb89d57ec794d109dd2eaaadb577d987
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-06-29 19:24:40 +0200

    added two components for github api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 //COMPONENTS:
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 //PAGES:
-import HomePage from "./pages/HomePage";
-import CandidateProfilePage from "./pages/CandidateProfilePage"
-import SignupCandidatePage from "./pages/SignupCandidatePage"
-import LoginCandidatePage from "./pages/LoginCandidatePage"
+import HomePage from './pages/HomePage';
+import CandidateProfilePage from './pages/CandidateProfilePage';
+import SignupCandidatePage from './pages/SignupCandidatePage';
+import LoginCandidatePage from './pages/LoginCandidatePage';
+import SearchUser from './components/SearchUser';
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-      {/* <Navbar /> */}
-          <Route exact path="/" element={<HomePage />}/>
-          <Route exact path="/profile/create" element={<CandidateProfilePage />}/>
-        </Routes>
+      {/* <SearchUser /> */}
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route
+          exact
+          path="/profile/create"
+          element={<CandidateProfilePage />}
+        />
+      </Routes>
       {/* <SignupCandidatePage />
-      <LoginCandidatePage /> */}
+      <LoginCandidatePage />  */}
     </div>
   );
 }
--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import ReposList from './ReposList';
 
 function SearchUser() {
   const [searchInput, setSearchInput] = useState('');
-  const [pinnedRepos, setPinnedRepos] = useState([]);
+  const [allRepos, setAllRepos] = useState([]);
+  const [userInfo, setUserInfo] = useState({});
+
+  const pinnedRepos = useMemo(() => {
+    return allRepos.filter((repo) => repo.stargazers_count > 0);
+  }, [allRepos]);
 
   const handleChange = (event) => {
     setSearchInput(event.target.value);
   };
   const handleClick = (event) => {
     event.preventDefault();
+
     axios
-      .get(`https://api.github.com/users/${searchInput}/repos`)
+      .get(`https://api.github.com/users/${searchInput}`)
       .then((response) => {
         // console.log('response.data', response.data);
-        setPinnedRepos(response.data);
+        setUserInfo(response.data);
+      });
+
+    axios
+      .get(`https://api.github.com/users/${searchInput}/repos?per_page=100`)
+      .then((response) => {
+        // console.log('response.data', response.data);
+        setAllRepos(response.data);
       });
   };
   return (
@@ -28,6 +41,7 @@ function SearchUser() {
         onChange={handleChange}
       />
       <button onClick={handleClick}>Search</button>
+      <p>{JSON.stringify(userInfo)}</p>
       <ReposList pinnedRepos={pinnedRepos} />
     </div>
   );
