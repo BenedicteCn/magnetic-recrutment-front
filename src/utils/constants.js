960994b04322967a269a97c675df251e74fd9b0c
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-07-06 18:04:07 +0200

    deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-const API_URL = process.env.API_URL || 'http://localhost:5005'
-const TOKEN_STORAGE_KEY = 'authToken'
+const TOKEN_STORAGE_KEY = "authToken";
 
-export { API_URL, TOKEN_STORAGE_KEY }
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005/";
+
+export { API_URL, TOKEN_STORAGE_KEY };
